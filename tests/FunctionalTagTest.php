fed30e2d1cf91f0399dbd723ff236febe9f99095
Tobias Nyholm <tobias.nyholm@gmail.com>
2015-12-08 22:52:12 +0100

    Added functional tests

---
+++ tests/FunctionalTagTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Cache\Doctrine\Tests;
+
+use Cache\Doctrine\CachePool;
+use Cache\IntegrationTests\Tests\TaggableCachePoolTest;
+use Doctrine\Common\Cache\MemcachedCache;
+
+class FunctionaTaglTest extends TaggableCachePoolTest
+{
+    function createCachePool()
+    {
+        $memcached = new \Memcached();
+        $memcached->addServer('localhost', 11211);
+        $doctrineCache = new MemcachedCache();
+        $doctrineCache->setMemcached($memcached);
+
+        return new CachePool($doctrineCache);
+    }
+}
