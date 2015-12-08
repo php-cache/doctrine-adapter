320478c29c872289807f96afc2635b922a314aa5
Aaron Scherer <aequasi@gmail.com>
2015-12-08 10:51:08 +0000

    Adding meta headers

--- tests/CachePoolTest.php
+++ tests/CachePoolTest.php
@@ -1,12 +1,12 @@
 <?php
 
-/**
- * This file is part of php-cache
+/*
+ * This file is part of php-cache\doctrine-adapter package.
  *
- * (c) Aaron Scherer <aequasi@gmail.com>
+ * (c) 2015-2015 Aaron Scherer <aequasi@gmail.com>
  *
- * This source file is subject to the license that is bundled
- * with this source code in the file LICENSE
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
  */
 
 namespace Cache\Doctrine\Tests;
